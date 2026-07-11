cbc7f9479670a05c9818a14a6cd56463d08b4a9f
Vivek Sharma <therealviveksharma@gamil.com>
2026-07-11 17:12:19 +0530

    bootstrap webpage

---
+++ 9_Bootstrap_Webpage/script.js
@@ -0,0 +1,11 @@
+const sidebar = document.querySelector(".sidebar");
+const openBtn = document.querySelector("#side");
+const closeBtn = document.querySelector(".X");
+
+openBtn.addEventListener("click", () => {
+    sidebar.classList.add("active");
+});
+
+closeBtn.addEventListener("click", () => {
+    sidebar.classList.remove("active");
+});
